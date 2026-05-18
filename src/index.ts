b16d48eb9b232ce3c240a7ac299405e0fe3bc523
Roger Chappel <miscanalysis@gmail.com>
2026-05-19 06:38:41 +1000

    feat: expose library API

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+export { explainWorkflow, renderExplanation } from "./explain.js";
+export { renderJson } from "./json.js";
+export { renderMarkdown } from "./markdown.js";
+export { renderReport } from "./output.js";
+export { scanWorkflows, summarizeWorkflow } from "./scan.js";
+export type {
+  ActionDeckReport,
+  OutputFormat,
+  PermissionSet,
+  ReviewItem,
+  WorkflowCommand,
+  WorkflowJob,
+  WorkflowSecretReference,
+  WorkflowSummary,
+  WorkflowTrigger
+} from "./types.js";
